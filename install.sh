784814d1b55cd1ecea1450ba9a61c09c86e6e6e9
Ben Bultman <bultman.ben@gmail.com>
2025-08-24 14:30:04 +0200

    Add install script

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
-echo "Installing alacrity config"
-mkdir -p ~/.config/alacritty
-cp alacritty.yml ~/.config/alacritty/alacritty.yml
+function import_config() {
+  local source="$1"
+  local dest="${2:-$HOME/.config}"
 
-echo "Installing tmux system config"
-cp tmux.conf /usr/local/etc/tmux.conf
+  echo ""
+  echo -en "Installing $dest/$source" >/dev/stderr
 
-echo "Done!"
+  if [[ -d "$dest/$source" || -f "$dest/$source" ]]; then
+    echo -en "\rInstalling $dest/$source -> skipped" >/dev/stderr
+    return
+  fi
+
+  cp -R "$source" "$dest"
+
+  echo -e "\rInstalling $dest/$source -> done" >/dev/stderr
+}
+
+import_config 'akind-git-commit-template'
+import_config 'akind.gitconfig'
+import_config 'foot'
+import_config 'helix'
+import_config 'fuzzel'
+import_config 'helix'
+import_config 'htop'
+import_config 'mako'
+import_config 'niri'
+import_config 'starship.toml'
+import_config 'zsh-base.sh'
+import_config '.zshrc' $HOME
+import_config '.ssh' $HOME
+echo ""
+echo "Remember to create ssh keys"
